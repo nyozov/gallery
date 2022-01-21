8a4f4e0e8fb21cdd86128eccef875d1c36185280
nikolay <n_yozov@hotmail.com>
2022-01-21 04:47:34 -0500

    fix modal not closing on delete

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,15 +2,19 @@ import handleDelete from "../hooks/handleDelete"
 
 export default function Modal({ selectedImg, setSelectedImg }){
   const handleClick = (e) => {
-    if(e.target.classList.contains('backdrop')){
+    if(e.target.classList.contains('backdrop', 'deleteButton')){
     setSelectedImg(null)
 
     }
   }
+  const deleteImg = ()=>{
+    handleDelete(selectedImg.id)
+    setSelectedImg(null)
+  }
   return (
     <div className="backdrop" onClick={handleClick}>
-      <img src={selectedImg} alt="enlarged pic" />
-      <button onClick={()=>handleDelete(selectedImg.url)}>delete</button>
+      <img src={selectedImg.url} alt="enlarged pic" />
+      <button className='deleteButton'onClick={deleteImg}>delete</button>
 
     </div>
   )
